79dcf091e30a346ca91b2128aa469ec18e94e517
Lauren Forcey <lauren4c@gmail.com>
2019-02-14 07:53:47 -0700

    Completed Assignment work for CP 2976

---
+++ node-greeting-app/date.js
@@ -0,0 +1,7 @@
+exports.valentinesDay = function() {
+  console.log("Valentine's Day is always February 14");
+};
+
+exports.presidentsDay = function() {
+  console.log("President's Day is the 3rd Monday of February");
+};
---
+++ node-greeting-app/main.js
@@ -0,0 +1,20 @@
+const greetings = require("./greeting.js");
+const date = require("./date.js");
+
+//"Hello"
+greetings.greetingEnglish();
+
+//"Konichiwa"
+greetings.greetingJapanese();
+
+//"Hola"
+greetings.greetingSpanish();
+
+//"Hallo"
+greetings.greetingIcelandic();
+
+//"Valentine's Day is always February 14"
+date.valentinesDay();
+
+//"President's Day is the 3rd Monday of February"
+date.presidentsDay();
